da1913de90798d56b2d54765efe372d52cbf0ab8
maxwellflitton <maxwellflitton@gmail.com>
2024-04-04 01:06:47 +0100

    adding feature support and wiping old nanoservices

--- src/docker_files/unpacking.rs
+++ src/docker_files/unpacking.rs
@@ -1,10 +1,36 @@
 //! Defines the actions around unpacking compressed Docker files from the manifest.
 use std::fs::File;
-use std::io::Read;
+use std::io::{self, Read, Seek, SeekFrom};
 use std::path::Path;
 use serde_json::Value;
 use tar::Archive;
 use flate2::read::GzDecoder;
+
+
+
+/// Checks if the given file is gzipped by reading its magic number.
+///
+/// # Arguments
+///
+/// * `file` - A mutable reference to the file to check.
+///
+/// # Returns
+///
+/// * `Result<bool, io::Error>` - `Ok(true)` if the file is gzipped, `Ok(false)` otherwise, or an `Err` on failure.
+///
+fn check_if_gzipped(file: &mut File) -> io::Result<bool> {
+    let mut magic_number = [0; 2];
+
+    // Seek to the start of the file and read the first two bytes
+    file.seek(SeekFrom::Start(0))?;
+    file.read_exact(&mut magic_number)?;
+
+    // Seek back to the start for subsequent operations on the file
+    file.seek(SeekFrom::Start(0))?;
+
+    // Check if the magic number matches gzip's 1F 8B
+    Ok(magic_number == [0x1f, 0x8b])
+}
 
 
 /// Reads a JSON file.
@@ -51,11 +77,21 @@ pub fn extract_layers(main_path: &str, unpack_path: &str) -> std::io::Result<Str
             let layer_path = base_path.join(layer.as_str().unwrap());
 
             // Extract the layer's tarball to a directory
-            let tar_file = File::open(layer_path)?;
-            // Create a GzDecoder to handle the gzip decompression
-            let decompressed = GzDecoder::new(tar_file);
-            let mut archive = Archive::new(decompressed);
-            archive.unpack(unpack_path).unwrap();
+            let mut tar_file = File::open(layer_path)?;
+
+            match check_if_gzipped(&mut tar_file)? {
+                true => {
+                    println!("Layer is gzipped");
+                    let decompressed = GzDecoder::new(tar_file);
+                    let mut archive = Archive::new(decompressed);
+                    archive.unpack(unpack_path).unwrap();
+                },
+                false => {
+                    println!("Layer is not gzipped");
+                    let mut archive = Archive::new(tar_file);
+                    archive.unpack(unpack_path).unwrap();
+                }
+            }
         }
     }
     else {
--- src/toml_operations/mod.rs
+++ src/toml_operations/mod.rs
@@ -8,6 +8,7 @@ use std::collections::{
 use std::path::PathBuf;
 use read::Nanoservice;
 use toml::{Table, Value};
+use crate::toml_operations::read::CargoToml;
 
 
 /// Gets all the nanoservices from the TOML files in the current directory.
@@ -26,14 +27,20 @@ pub fn get_all_nanoservices()
 
     for cargo_path in cargo_paths {
         println!("cargo_path: {:?}", cargo_path);
-        let cargo_toml = read::read_toml(cargo_path.to_str().unwrap());
+        let mut cargo_toml = read::read_toml(cargo_path.to_str().unwrap());
+        wipe_nanoservices(&mut cargo_toml);
 
         let mut buffer = Vec::new();
 
-        // break the loop if there are no nanoservices in the cargo file
         let nanos = match cargo_toml.nanoservices {
             Some(nanos) => nanos,
-            None => continue
+            None => {
+                // break the loop if there are no nanoservices in the cargo file
+                // write the cargo file back to the disk as the nanoservices might
+                // have been wiped
+                read::write_toml(cargo_path.to_str().unwrap(), cargo_toml);
+                continue;
+            }
         };
 
         // loop through the nanoservices and add them to the buffer and all nanoservices hashset
@@ -45,8 +52,37 @@ pub fn get_all_nanoservices()
             cargo_dependencies.insert(cargo_path, buffer);
         }
     }
-
     return (cargo_dependencies, all_nanoservices)
+}
+
+
+/// Wipes the nanoservices from the Cargo.toml dependencies.
+///
+/// # Arguments
+/// * `cargo_toml_file` - A mutable reference to the CargoToml struct.
+///
+/// # Returns
+/// None
+fn wipe_nanoservices(cargo_toml_file: &mut CargoToml) {
+    println!("Wiping nanoservices");
+    for (name, value) in cargo_toml_file.dependencies.clone() {
+        println!("name: {}", name);
+        match value {
+            Value::Table(table) => {
+                match table.get("path") {
+                    Some(path) => {
+                        let path = path.as_str().unwrap();
+                        if path.contains(".nanoservices_cache") {
+                            println!("Removing nanoservice: {}", name);
+                            cargo_toml_file.dependencies.remove(&name);
+                        }
+                    },
+                    None => ()
+                }
+            },
+            _ => ()
+        }
+    }
 }
 
 
@@ -75,7 +111,67 @@ pub fn config_cargo(path: PathBuf, nanos:  Vec<(String, Nanoservice)>) {
                 ).to_str().unwrap().to_string()
             )
         );
+        // add the features to the table if they exist
+        match nanoservice.features {
+            Some(features) => {
+                nanoservice_table.insert(
+                    "features".to_string(),
+                    Value::Array(
+                        features.iter().map(|f|
+                                Value::String(f.to_string())
+                        ).collect()
+                    )
+                );
+            },
+            None => ()
+        }
         cargo_toml.dependencies.insert(name, toml::Value::Table(nanoservice_table));
     }
     read::write_toml(path.to_str().unwrap(), cargo_toml);
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::toml_operations::read::{
+        CargoToml,
+        Package
+    };
+
+    #[test]
+    fn test_wipe_nanoservices() {
+
+        let mut dependencies = HashMap::new();
+        dependencies.insert("test".to_string(), Value::Table(Table::new()));
+        dependencies.insert("test2".to_string(), Value::Table(Table::new()));
+
+        let mut table = Table::new();
+        table.insert("path".to_string(), Value::String("test".to_string()));
+        dependencies.insert("test3".to_string(), Value::Table(table));
+
+        let mut nanoservice_table = Table::new();
+        nanoservice_table.insert("path".to_string(), Value::String(
+            ".nanoservices_cache/one/two".to_string())
+        );
+        dependencies.insert("test4".to_string(), Value::Table(nanoservice_table));
+
+        let mut cargo_toml = CargoToml {
+            package: Package {
+                name: "test".to_string(),
+                version: "0.1.0".to_string(),
+                edition: "2018".to_string()
+            },
+            dependencies,
+            nanoservices: None
+        };
+        wipe_nanoservices(&mut cargo_toml);
+
+        assert_eq!(cargo_toml.dependencies.len(), 3);
+        assert_eq!(cargo_toml.dependencies.contains_key("test"), true);
+        assert_eq!(cargo_toml.dependencies.contains_key("test2"), true);
+        assert_eq!(cargo_toml.dependencies.contains_key("test3"), true);
+    }
+
 }
